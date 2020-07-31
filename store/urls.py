56e501c91fae5ad1228f27e462ee9c85f978b238
janric <janricmalate@gmail.com>
2020-07-31 21:43:10 +0800

    part 3 end

--- store/urls.py
+++ store/urls.py
@@ -17,12 +17,14 @@ from .views import (
 	store,
 	cart,
 	checkout,
-    updateItem
+    updateItem,
+    processOrder
 )
 
 urlpatterns = [
     path('', store, name='store'),
     path('cart/', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
-     path('update_item/', updateItem, name='update_item')
+    path('update_item/', updateItem, name='update_item'),
+    path('process_order/', processOrder, name='process_order'),
 ]
